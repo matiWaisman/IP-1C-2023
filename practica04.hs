4b8650148b14171b59050c162c5a9bea5e15bcfd
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-04-18 22:16:15 -0300

    Terminados ejercicios 5,6,7,8

--- practica04.hs
+++ practica04.hs
@@ -1,40 +1,70 @@
 -- Ejercicio 1
 
-fib :: Int -> Int 
-fib n | n == 0 = 0
-      | n == 1 = 1
-      | otherwise = fib (n - 1) + fib (n - 2)
+fib :: Int -> Int
+fib n
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = fib (n - 1) + fib (n - 2)
 
 -- Ejercicio 2
 
-parteEntera :: Float -> Int 
-parteEntera x | 0 <= x && x < 1 = 0
-              | otherwise = parteEntera(x - 1) + 1
+parteEntera :: Float -> Int
+parteEntera x
+  | 0 <= x && x < 1 = 0
+  | otherwise = parteEntera (x - 1) + 1
 
 -- Ejercicio 3
 
 esDivisor :: Int -> Int -> Bool
-esDivisor n d | n == 0 = True
-              | n < 0 = False
-              | otherwise = esDivisor (n - d) d
+esDivisor n d
+  | n == 0 = True
+  | n < 0 = False
+  | otherwise = esDivisor (n - d) d
 
 -- Ejercicio 4
 
-sumaImpares :: Int -> Int 
-sumaImpares n | n == 1 = 1
-              | otherwise = sumaImpares(n-1) + nesimoImpar n
-
---Ejercicio 5
+sumaImpares :: Int -> Int
+sumaImpares n
+  | n == 1 = 1
+  | otherwise = sumaImpares (n - 1) + nesimoImpar n
 
 nesimoImpar :: Int -> Int
-nesimoImpar n | n == 1 = 1 
-              | otherwise = nesimoImpar(n-1) + 2
+nesimoImpar n
+  | n == 1 = 1
+  | otherwise = nesimoImpar (n - 1) + 2
+
+-- Ejercicio 5
+
+medioFact :: Int -> Int
+medioFact n
+  | n == 0 = 1
+  | n == 1 = 1
+  | n == 2 = 2
+  | otherwise = n * medioFact (n - 2)
+
+-- Ejercicio 6
+
+sumaDigitos :: Int -> Int
+sumaDigitos n
+  | n < 10 = n
+  | otherwise = n `mod` 10 + sumaDigitos (n `div` 10)
 
 -- Ejercicio 7
 
 todosDigitosIguales :: Int -> Bool
-todosDigitosIguales n | n `mod` 10 /= n `div` 10 = False
-                      | n < 10 = True 
-                      | n `div` 10 >= 10 = todosDigitosIguales (n `div` 10)
+todosDigitosIguales n
+  | n < 10 = True
+  | n `mod` 10 /= (n `div` 10) `mod` 10 = False
+  | otherwise = todosDigitosIguales (n `div` 10)
 
 -- Ejercicio 8
+
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito i n
+  | cantidadDeDigitos n == i = n `mod` 10
+  | otherwise = iesimoDigito i (n `div` 10)
+
+cantidadDeDigitos :: Int -> Int
+cantidadDeDigitos n
+  | n < 10 = 1
+  | otherwise = 1 + cantidadDeDigitos (n `div` 10)
