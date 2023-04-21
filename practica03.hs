47687f22d3bde2693b50e5a9586ebc597228cf30
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-04-21 20:00:27 -0300

    Avances + Cambio Int por Integer

--- practica03.hs
+++ practica03.hs
@@ -1,55 +1,55 @@
 import Distribution.Simple.Setup (trueArg)
 
-doubleMe :: Int -> Int
+doubleMe :: Integer -> Integer
 doubleMe x = x + x
 
 -- Ejercicio 1
 
 -- Punto A
-f :: Int -> Int
+f :: Integer -> Integer
 f n
   | n == 1 = 8
   | n == 4 = 138
   | n == 16 = 16
 
 -- Punto B
-g :: Int -> Int
+g :: Integer -> Integer
 g n
   | n == 8 = 16
   | n == 16 = 4
   | n == 131 = 1
 
 -- Punto C
-h :: Int -> Int
+h :: Integer -> Integer
 h n = f (g n)
 
-k :: Int -> Int
+k :: Integer -> Integer
 k n = g (f n)
 
 -- Punto 2
 
 -- Punto A
 
-absoluto :: Int -> Int
+absoluto :: Integer -> Integer
 absoluto n
   | n > 0 = n
   | n < 0 = n * (-1)
 
 -- Punto B
 
-maximoAbsoluto :: Int -> Int -> Int
+maximoAbsoluto :: Integer -> Integer -> Integer
 maximoAbsoluto x y
   | absoluto x > absoluto y = absoluto x
   | absoluto x < absoluto y = absoluto y
 
 -- Punto C
 
-maximo :: Int -> Int -> Int
+maximo :: Integer -> Integer -> Integer
 maximo x y
   | x > y = x
   | x < y = y
 
-maximo3 :: Int -> Int -> Int -> Int
+maximo3 :: Integer -> Integer -> Integer -> Integer
 maximo3 x y z
   | x == maximo x y && x == maximo x z = x
   | y == maximo x y && x == maximo y z = y
@@ -86,8 +86,8 @@ ambosSon0P 0 0 = True -- Como uso el otherwise aca? Asi no tengo que hacer los 4
 
 -- Punto F
 
-mismoIntervalo :: Float -> Float -> Bool
-mismoIntervalo x y
+mismoIntegerervalo :: Float -> Float -> Bool
+mismoIntegerervalo x y
   | x <= 3 && y <= 3 = True
   | (x > 3 && x <= 7) && (y > 3 && x <= 7) = True
   | x > 7 && y > 7 = True
@@ -95,8 +95,8 @@ mismoIntervalo x y
 
 -- Punto G
 
-sumaDistintos :: Int -> Int -> Int -> Int
-sumaDistintos x y z
+sumaDistIntegeros :: Integer -> Integer -> Integer -> Integer
+sumaDistIntegeros x y z
   | x == y && y == z = x
   | x == y && x /= z = x + z
   | x /= y && x == z = x + y
@@ -105,24 +105,24 @@ sumaDistintos x y z
 
 -- Punto H
 
-esMultiploDe :: Int -> Int -> Bool
+esMultiploDe :: Integer -> Integer -> Bool
 esMultiploDe x y
   | mod x y == 0 = True
   | otherwise = False
 
 -- Punto I
 
-digitoUnidades :: Int -> Int
+digitoUnidades :: Integer -> Integer
 digitoUnidades x = x `mod` 10
 
 -- Punto J
 
-digitoDecenas :: Int -> Int
+digitoDecenas :: Integer -> Integer
 digitoDecenas x = (x `div` 10) `mod` 10
 
 -- Ejercicio 3
 
-estanRelacionados :: Int -> Int -> Bool
+estanRelacionados :: Integer -> Integer -> Bool
 estanRelacionados a b
   | a == 0 && b == 0 = False
   | a * a + a * b * k == 0 = True
@@ -134,12 +134,12 @@ estanRelacionados a b
 
 -- Punto A
 
-prodInt :: (Int, Int) -> (Int, Int) -> Int
-prodInt (x1, y1) (x2, y2) = x1 * x1 + y1 * y2
+prodInteger :: (Integer, Integer) -> (Integer, Integer) -> Integer
+prodInteger (x1, y1) (x2, y2) = x1 * x1 + y1 * y2
 
 -- Punto B
 
-todoMenor :: (Int, Int) -> (Int, Int) -> Bool
+todoMenor :: (Integer, Integer) -> (Integer, Integer) -> Bool
 todoMenor (x1, y1) (x2, y2)
   | x1 < x2 && y1 < y2 = True
   | otherwise = False
@@ -151,24 +151,24 @@ distanciaPuntos (x1, y1) (x2, y2) = sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
 -- Punto D
 
-sumaTerna :: (Int, Int, Int) -> Int
+sumaTerna :: (Integer, Integer, Integer) -> Integer
 sumaTerna (x1, x2, x3) = x1 + x2 + x3
 
 -- Punto F
 
-sumarSoloMultiplos :: (Int, Int, Int) -> Int -> Int
+sumarSoloMultiplos :: (Integer, Integer, Integer) -> Integer -> Integer
 sumarSoloMultiplos (x1, x2, x3) n
   | n < 0 = 0
   | n > 0 = determinarMultiplo x1 n + determinarMultiplo x2 n + determinarMultiplo x3 n
 
-determinarMultiplo :: Int -> Int -> Int
+determinarMultiplo :: Integer -> Integer -> Integer
 determinarMultiplo x n
   | mod x n == 0 = x
   | mod x n /= 0 = 0
 
 -- Punto F
 
-posPrimerPar :: (Int, Int, Int) -> Int
+posPrimerPar :: (Integer, Integer, Integer) -> Integer
 posPrimerPar (x1, x2, x3)
   | esMultiploDe x1 2 = 0
   | esMultiploDe x2 2 = 1
@@ -187,24 +187,24 @@ invertir (a, b) = (b, a)
 
 -- Ejercicio 5
 
-todosMenores :: (Int, Int, Int) -> Bool
+todosMenores :: (Integer, Integer, Integer) -> Bool
 todosMenores (n1, n2, n3)
   | f n1 > g n1 && f n2 > g n2 && f n3 > g n3 = True
   | otherwise = False
 
-f5 :: Int -> Int
+f5 :: Integer -> Integer
 f5 n
   | n <= 7 = n ^ 2
   | otherwise = 2 * n - 1
 
-g5 :: Int -> Int
+g5 :: Integer -> Integer
 g5 n
   | esMultiploDe n 2 = n `div` 2
   | otherwise = 3 * n + 1
 
 -- Ejercicio 6
 
-bisiesto :: Int -> Bool
+bisiesto :: Integer -> Bool
 bisiesto y
   | not (esMultiploDe y 4) || (esMultiploDe y 100 && not (esMultiploDe y 400)) = False
   | otherwise = True
@@ -216,11 +216,11 @@ distanciaManhattan (p1, p2, p3) (q1, q2, q3) = abs (p1 - q1) + abs (p2 - q2) + a
 
 -- Ejercicio 8
 
-comparar :: Int -> Int -> Int
+comparar :: Integer -> Integer -> Integer
 comparar a b
   | sumaUltimoDosDigitos a < sumaUltimoDosDigitos b = 1
   | sumaUltimoDosDigitos a > sumaUltimoDosDigitos b = -1
   | sumaUltimoDosDigitos a == sumaUltimoDosDigitos b = 0
 
-sumaUltimoDosDigitos :: Int -> Int
+sumaUltimoDosDigitos :: Integer -> Integer
 sumaUltimoDosDigitos x = digitoUnidades x + digitoDecenas x
--- practica04.hs
+++ practica04.hs
@@ -1,6 +1,6 @@
 -- Ejercicio 1
 
-fib :: Int -> Int
+fib :: Integer -> Integer
 fib n
   | n == 0 = 0
   | n == 1 = 1
@@ -8,14 +8,14 @@ fib n
 
 -- Ejercicio 2
 
-parteEntera :: Float -> Int
+parteEntera :: Float -> Integer
 parteEntera x
   | 0 <= x && x < 1 = 0
   | otherwise = parteEntera (x - 1) + 1
 
 -- Ejercicio 3
 
-esDivisor :: Int -> Int -> Bool
+esDivisor :: Integer -> Integer -> Bool
 esDivisor n d
   | n == 0 = True
   | n < 0 = False
@@ -23,19 +23,19 @@ esDivisor n d
 
 -- Ejercicio 4
 
-sumaImpares :: Int -> Int
+sumaImpares :: Integer -> Integer
 sumaImpares n
   | n == 1 = 1
   | otherwise = sumaImpares (n - 1) + nesimoImpar n
 
-nesimoImpar :: Int -> Int
+nesimoImpar :: Integer -> Integer
 nesimoImpar n
   | n == 1 = 1
   | otherwise = nesimoImpar (n - 1) + 2
 
 -- Ejercicio 5
 
-medioFact :: Int -> Int
+medioFact :: Integer -> Integer
 medioFact n
   | n == 0 = 1
   | n == 1 = 1
@@ -44,14 +44,14 @@ medioFact n
 
 -- Ejercicio 6
 
-sumaDigitos :: Int -> Int
+sumaDigitos :: Integer -> Integer
 sumaDigitos n
   | n < 10 = n
   | otherwise = n `mod` 10 + sumaDigitos (n `div` 10)
 
 -- Ejercicio 7
 
-todosDigitosIguales :: Int -> Bool
+todosDigitosIguales :: Integer -> Bool
 todosDigitosIguales n
   | n < 10 = True
   | n `mod` 10 /= (n `div` 10) `mod` 10 = False
@@ -59,57 +59,65 @@ todosDigitosIguales n
 
 -- Ejercicio 8
 
-iesimoDigito :: Int -> Int -> Int
+iesimoDigito :: Integer -> Integer -> Integer
 iesimoDigito i n
   | cantidadDeDigitos n == i = n `mod` 10
   | otherwise = iesimoDigito i (n `div` 10)
 
-cantidadDeDigitos :: Int -> Int
+cantidadDeDigitos :: Integer -> Integer
 cantidadDeDigitos n
   | n < 10 = 1
   | otherwise = 1 + cantidadDeDigitos (n `div` 10)
 
 -- Ejercicio 9
 
--- esCapicua :: Int -> Bool
+-- esCapicua :: Integer -> Bool
 -- esCapicua n
---  | n < 10 = True
---  | n `div` (10 ^ (cantidadDeDigitos n - 1)) /= n `mod` 10 = False
---  | otherwise = esCapicua como hago para volver a llamar a la funcion pero sacandome el primero y el ultimo sin pasarlo a strings
+
+-- | n < 10 = True
+-- | n `div` (10 ^ (cantidadDeDigitos n - 1)) /= n `mod` 10 = False
+-- | otherwise = esCapicua
 
 -- Ejercicio 10
 -- Los que terminan en s son los ejercicios resueltos con forma de sumatoria y los que terminan en g son usando el termino general probado por induccion
 
-f1s :: Int -> Int
+f1s :: Integer -> Integer
 f1s n
   | n == 0 = 2 ^ n
   | otherwise = f1s (n - 1) + 2 ^ n
 
-f1g :: Int -> Int
+f1g :: Integer -> Integer
 f1g n = 2 ^ (n + 1) - 1
 
-f2s :: Int -> Int -> Int
+f2s :: Integer -> Integer -> Integer
 f2s n q
   | n == 1 = q ^ n
   | otherwise = f2s (n - 1) q + q ^ n
 
-f2g :: Int -> Float -> Float
+f2g :: Integer -> Float -> Float
 f2g n q
-  | q == 1 || q == -1 = fromIntegral n + 1 -- Como sacar el fromIntegral
+  | q == 1 || q == -1 = fromIntegral n + 1
   | otherwise = (q ^ (n + 1) - 1) / q - 1 -- Verificar
 
-f3s :: Int -> Float -> Float
-f3s n q
-  | n == 1 = q
-  | otherwise = f3s ((2 * n) - 1) q + q ^ n -- Como resolver sin necesidad de pasarle un parametro extra (2n)
+f3s :: Integer -> Float -> Float
+f3s n q = sumatoria2n (2 * n) q
+  where
+    sumatoria2n :: Integer -> Float -> Float
+    sumatoria2n dosN q
+      | dosN == 1 = q
+      | otherwise = f3s (dosN - 1) q + q ^ dosN
 
--- f3g :: Int -> Float -> Float To do
--- f3g n q
+-- f3g :: Integer -> Float -> Float To do
 
--- f4s :: Int -> Float -> Float To do
--- f4s n q
+f4s :: Integer -> Float -> Float
+f4s n q = sumatoriaF4 n (2 * n) q
+  where
+    sumatoriaF4 :: Integer -> Integer -> Float -> Float
+    sumatoriaF4 n dosN q
+      | dosN == n = q ^ n
+      | otherwise = sumatoriaF4 n (dosN - 1) q + q ^ dosN
 
-f4g :: Int -> Float -> Float
+f4g :: Integer -> Float -> Float
 f4g n q
   | q == 1 = fromIntegral n - 1
   | otherwise = ((q ^ (2 * n + 1)) - q ^ n) / (q - 1)
@@ -117,61 +125,122 @@ f4g n q
 -- Ejercicio 11 Falta verificar
 -- Punto A
 
-eAprox :: Int -> Float
+eAprox :: Integer -> Float
 eAprox n
   | n == 0 = 1 / fromIntegral (factorial n)
   | otherwise = eAprox (n - 1) + 1 / fromIntegral (factorial n)
 
-factorial :: Int -> Int
+factorial :: Integer -> Integer
 factorial n
   | n == 0 = 1
   | otherwise = factorial (n - 1) * n
 
 -- Punto B
 
--- eAproxDiez :: Int -> Float
+-- eAproxDiez :: Integer -> Float
 -- eAproxDiez n
 
--- Ejercicio 12 Preguntar consigna
+-- Ejercicio 12 Falta verificar
 
-raizDe2Aprox :: Int -> Float
+raizDe2Aprox :: Integer -> Float
 raizDe2Aprox n
   | n == 1 = 2
   | otherwise = 2 + (1 / raizDe2Aprox (n - 1))
 
 -- Ejercicio 13
 
-dobleSumatoria :: Int -> Int -> Int
+dobleSumatoria :: Integer -> Integer -> Integer
 dobleSumatoria n m
   | n == 1 = segundaSumatoria 1 m
   | otherwise = dobleSumatoria (n - 1) m + segundaSumatoria n m
 
-segundaSumatoria :: Int -> Int -> Int
+segundaSumatoria :: Integer -> Integer -> Integer
 segundaSumatoria i m
   | m == 1 = i ^ m
   | otherwise = segundaSumatoria i (m - 1) + i ^ m
 
 -- Ejercicio 14 Preguntar consigna
 
--- sumaPotencias :: Int ->Int ->Int ->Int
+-- sumaPotencias :: Integer ->Integer ->Integer ->Integer
 -- sumaPotencias q n m
 
 -- Ejercicio 15
 
-sumaRacionales :: Int -> Int -> Float
+sumaRacionales :: Integer -> Integer -> Float
 sumaRacionales n m
-  | n == 1 = sumaInterna 1 m
-  | otherwise = sumaRacionales (n - 1) m + sumaInterna n m
+  | n == 1 = sumaIntegererna 1 m
+  | otherwise = sumaRacionales (n - 1) m + sumaIntegererna n m
 
-sumaInterna :: Int -> Int -> Float
-sumaInterna p m
+sumaIntegererna :: Integer -> Integer -> Float
+sumaIntegererna p m
   | m == 1 = fromIntegral p / fromIntegral m
-  | otherwise = sumaInterna p (m - 1) + fromIntegral p / fromIntegral m
+  | otherwise = sumaIntegererna p (m - 1) + fromIntegral p / fromIntegral m
 
--- Ejercicio 16 Como resolverlos sin pasar un parametro
+-- Ejercicio 16
 
--- menorDivisor :: Int -> Int --
--- menorDivisor n
+menorDivisor :: Integer -> Integer
+menorDivisor n = encontrarMenorDivisor n 2
 
--- esPrimo :: Int ->Bool
--- esPrimo n
+encontrarMenorDivisor :: Integer -> Integer -> Integer
+encontrarMenorDivisor n i
+  | n == 1 = 1
+  | n /= i && n `mod` i == 0 = i
+  | n == i = i
+  | otherwise = encontrarMenorDivisor n (i + 1)
+
+esPrimo :: Integer -> Bool
+esPrimo n
+  | encontrarMenorDivisor n 2 == n = True
+  | otherwise = False
+
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos n1 n2
+  | encontrarMenorDivisorComun n1 n2 2 == 1 = True
+  | otherwise = False
+
+encontrarMenorDivisorComun :: Integer -> Integer -> Integer -> Integer
+encontrarMenorDivisorComun n1 n2 i
+  | i > n1 || i > n2 = 1
+  | n1 `mod` i == 0 && n2 `mod` i == 0 = i
+  | otherwise = encontrarMenorDivisorComun n1 n2 (i + 1)
+
+-- nEsimoPrimo :: Integer -> Integer To Do
+-- nesimoPrimo n
+
+-- Ejercicio 17 Preguntar, idea: Hacer una funcion recursiva que me vaya dando el numero de fibo metiendole un iterador. Si me da igual devuelvo true, si el numero de fibonacci se hace 50 unidades mas grande corto el loop para que no sea infinito
+
+esFibonacci :: Integer -> Bool
+esFibonacci n = iterarFibo n 0
+  where
+    iterarFibo :: Integer -> Integer -> Bool
+    iterarFibo n i
+      | fib i == n = True
+      | n - fib i < -100 = False -- Esto lo hago para cortar el loop, si por ejemplo me dan 500 i i = 14 -> Fib 14 = 377 = 15 -> Fib 15 = 610 500 - 610 = 100, corto
+      | otherwise = iterarFibo n (i + 1)
+
+-- Ejercicio 18
+
+mayorDigitoPar :: Integer -> Integer
+mayorDigitoPar n = encontrarDigitoMayorPar n (-1)
+  where
+    encontrarDigitoMayorPar :: Integer -> Integer -> Integer
+    encontrarDigitoMayorPar n a
+      | (n >= 10) && (n `mod` 10 >= a) && (n `mod` 10) `mod` 2 == 0 = encontrarDigitoMayorPar (n `div` 10) (n `mod` 10)
+      | n < 10 && (n `mod` 10) `mod` 2 == 0 && n `mod` 10 > a = n `mod` 10 -- Evaluo el ultimo caso para cortar la recursion
+      | n < 10 && ((n `mod` 10) `mod` 2 /= 0 || n `mod` 10 <= a) = a -- Evaluo el ultimo caso para cortar la recursion
+      | otherwise = encontrarDigitoMayorPar (n `div` 10) a
+
+-- Ejercicio 19 Preguntar como generar los primos con el ejercicio 16 D
+
+-- esSumaInicialDePrimos :: Integer ->Bool
+-- esSumaInicialDePrimos n
+
+-- Ejercicio 20 Preguntar
+
+-- tomaValorMax :: Integer -> Integer -> Integer
+-- tomaValorMax n1 n2
+
+-- Ejercicio 21 Preguntar
+
+-- pitagoras :: Integer -> Integer -> Integer -> Integer
+-- pitagoras m n r
