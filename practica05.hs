b390c6550d5c5a5bd09e7e48af01704d983e05e1
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-04-26 11:05:21 -0300

    Terminados la mayoria del ejercicio 3

--- practica05.hs
+++ practica05.hs
@@ -87,9 +87,40 @@ productoria (x : xs)
   | null xs = x
   | otherwise = x * productoria xs
 
--- maximo :: [Integer] -> Integer
--- maximo (x : xs)
+maximo :: [Integer] -> Integer
+maximo (x : xs) = calcularMaximo x xs
+  where
+    calcularMaximo :: Integer -> [Integer] -> Integer
+    calcularMaximo max (x : xs)
+      | null xs && max > x = max
+      | null xs && max < x = x
+      | max < x = calcularMaximo x xs
+      | otherwise = calcularMaximo max xs
 
--- sumarN :: Integer -> [Integer] -> [Integer]
--- sumarN n (x : xs)
---  | null xs = x + n
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [] = []
+sumarN n (x : xs) = (x + n) : sumarN n xs
+
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero (x : xs) = x + x : sumarN x xs
+
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [] = []
+sumarElUltimo (x : xs) = x + conseguirElUltimo (x : xs) : sumarElUltimo xs -- : sumarN conseguirElUltimo (x : xs) xs tambien deberia funcionar
+  where
+    conseguirElUltimo :: [Integer] -> Integer -- Podria usar last pero lo hago por el amor al arte!!!
+    conseguirElUltimo (x : xs)
+      | null xs = x
+      | otherwise = conseguirElUltimo xs
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x : xs)
+  | x `mod` 2 == 0 = x : pares xs
+  | otherwise = pares xs
+
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN n [] = []
+multiplosDeN n (x : xs)
+  | x `mod` n == 0 = x : multiplosDeN n xs
+  | otherwise = multiplosDeN n xs
