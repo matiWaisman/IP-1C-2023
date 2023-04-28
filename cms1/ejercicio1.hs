4bdeb1681866b145fd1dcc1850ba1978e2015e82
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-04-28 18:11:28 -0300

    Ejercicio 1 cms terminado perfecto

--- cms1/ejercicio1.hs
+++ cms1/ejercicio1.hs
@@ -1,37 +1,25 @@
 main :: IO ()
 main = do
   x <- readLn
-  print (sumaMenosQueMax (x :: (Integer, Integer, Integer)))
+  print (sumaMenosQueMax (x :: (Int, Int, Int)))
 
-sumaMenosQueMax :: (Integer, Integer, Integer) -> Bool
-sumaMenosQueMax (t0, t1, t2)
-  | maximo t0 t1 t2 > (minimo t0 t1 t2 + medio t0 t1 t2) = True
-  | otherwise = False
+sumaMenosQueMax :: (Int, Int, Int) -> Bool
+sumaMenosQueMax t = maximo t > (minimo t + medio t)
 
-maximo :: Integer -> Integer -> Integer -> Integer
-maximo a b c
-  | a == calcularMaximo a b && a == calcularMaximo a c = a
-  | b == calcularMaximo a b && b == calcularMaximo b c = b
-  | c == calcularMaximo a c && c == calcularMaximo b c = c
-  where
-    calcularMaximo :: Integer -> Integer -> Integer
-    calcularMaximo a b
-      | a > b = a
-      | a < b = b
+maximo :: (Int, Int, Int) -> Int
+maximo (a, b, c)
+  | a >= b && a >= c = a
+  | b >= a && b >= c = b
+  | c >= b && c >= a = c
 
-minimo :: Integer -> Integer -> Integer -> Integer
-minimo a b c
-  | a == calcularMinimo a b && a == calcularMinimo a c = a
-  | b == calcularMinimo a b && b == calcularMinimo b c = b
-  | c == calcularMinimo a c && c == calcularMinimo b c = c
-  where
-    calcularMinimo :: Integer -> Integer -> Integer
-    calcularMinimo a b
-      | a < b = a
-      | a > b = b
+minimo :: (Int, Int, Int) -> Int
+minimo (a, b, c)
+  | a <= b && a <= c = a
+  | b <= a && b <= c = b
+  | c <= b && c <= a = c
 
-medio :: Integer -> Integer -> Integer -> Integer
-medio a b c
+medio :: (Int, Int, Int) -> Int
+medio (a, b, c)
   | (a <= b && b <= c) || (c <= b && b <= a) = b
   | (b <= a && a <= c) || (c <= a && a <= c) = a
   | otherwise = c
