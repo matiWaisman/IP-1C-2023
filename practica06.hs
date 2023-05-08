eaa17edd00c695080736dcf446574cbb764bedca
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-05-08 16:54:40 -0300

    Update practica06.hs

--- practica06.hs
+++ practica06.hs
@@ -1,1 +1,17 @@
+-- Ejercicio 1
+1) El objetivo de realizar testing de un software verificar que los casos de uso devuelvan lo que la especificación asegura buscando fallas para solucionarlas.
+
+2)
+
+3) El testing de caja negra consiste en generar casos de prueba en base a la especificación y no la implementación y evaluarlos.
+
+-- Ejercicio 2 
+
+1) Los factores van a ser la longitud de cada uno de los 3 catetos. 
+
+2) 
+
+3) Una categoría puede ser si los 3 lados miden lo mismo, si dos de los tres miden lo mismo, si todos miden distinto, si la suma de dos de los lados son mayores (se puede formar un triangulo) o menores (no se puede formar un triangulo)
+
+4 y 5) Algunos choices pueden ser: si los 3 lados son iguales es unico si la suma de dos de los lados son menores a el tercero lo clasifico como error, si alguno de los lados son 0 o negativo son errores. 
 
