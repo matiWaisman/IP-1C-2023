d51e91430f0b7751cc79956fb7cd90a529417ed1
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2023-05-26 18:40:22 -0300

    Filas parecidas 90/100 y fibonacci 10/100

--- cms2/fibonacciNoRecursivo.py
+++ cms2/fibonacciNoRecursivo.py
@@ -1,11 +1,15 @@
 import sys
+from typing import List
 
 def fibonacciNoRecursivo(n: int) -> int:
   res: int = 0
-  if(n == 0 or n == 1): res = 1
-  else: res = 
-  return 0
-
+  if n == 0 or n == 1: return 1
+  ultimos: List[int] = [1,1]
+  for i in range(2, n + 1, 1):
+    res =  ultimos[0] + ultimos[1]
+    ultimos[0] = ultimos[1]
+    ultimos[1] = res
+  return res
 
 if __name__ == '__main__':
   x = int(input())
---
+++ cms2/filasParecidas.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# Aclaración: Debido a la versión de Python del CMS, para el tipo Lista, la sintaxis de la definición de tipos que deben usar es la siguiente:
+# l: List[int]  <--Este es un ejemplo para una lista de enteros.
+# Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
+def filasParecidas(matriz: List[List[int]]) -> bool :
+  diferencia_esperada: int = matriz[1][0] - matriz[0][0]
+  if len(matriz) == 1: return True
+  res: bool = True
+  for i in range(0, len(matriz) - 1, 1): 
+    for j in range(0, len(matriz[i]), 1): 
+      if((matriz[i + 1][j] - matriz[i][j]) != diferencia_esperada): res = False
+
+  return res
+
+
+if __name__ == '__main__':
+  filas = int(input())
+  columnas = int(input())
+ 
+  matriz = []
+ 
+  for i in range(filas):         
+    fila = input()
+    if len(fila.split()) != columnas:
+      print("Fila " + str(i) + " no contiene la cantidad adecuada de columnas")
+    matriz.append([int(j) for j in fila.split()])
+  
+  print(filasParecidas(matriz))
